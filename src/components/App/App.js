20f98158fc94cec6193786c1c9014fc8a5854e1a
Chris Crabtree <chris.crabtree@matillion.com>
2021-11-26 13:11:58 +0000

    Stuff

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,11 +6,22 @@ import Dashboard from "../Dashboard/Dashboard";
 import Preferences from "../Preferences/Preferences";
 import Login from "../Login/Login";
 
+function getToken() {
+    const stringToken = sessionStorage.getItem('token');
+    const userToken = JSON.parse(stringToken);
+    return userToken.token
+
+}
+
+function setToken(userToken) {
+    sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
 function App() {
-    const [token, setToken] = useState();
+    const token = getToken();
 
     if (!token) {
-        return <Login setToken={setToken}></Login>
+        return <Login setToken={setToken}/>
     }
 
     return (
